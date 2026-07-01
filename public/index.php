86df6d2ede09b3913db0a919295836666ee8d1b1
mors6960 <mors6960@gmail.com>
2026-07-02 00:26:56 +0530

    Fix dotenv in public/index.php for Render

--- public/index.php
+++ public/index.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+    $dotenv->load();
+}
+foreach (getenv() ?: [] as $k => $v) { if (empty($_ENV[$k])) $_ENV[$k] = $v; }
 
 header('Content-Type: application/json');
 header('X-Content-Type-Options: nosniff');
